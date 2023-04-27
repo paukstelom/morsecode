f9c0ec1808dab572bee6f9a929ff4ba8326247cc
paukstello <paukstelom@gmail.com>
2023-04-27 13:09:51 +0300

    Polished console version of morse code converter

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from morse_code_dict import morse_code_dict
+from utils import morse_code_dict, logo, ask_to_continue
 
-running = True
+print(logo)
+running = ask_to_continue('Begin? (Y/N): ')
 while running:
-    text_input = input('Enter text:\n').upper().strip()
+    text_input = input('Enter text to convert to morse code:\n').upper().strip()
 
     morse_code_output = ''
     for char in text_input:
         if char in morse_code_dict.keys():
             morse_code_output += morse_code_dict[char]
+    print(f'Morse code:')
     print(morse_code_output.rstrip())
-
+    running = ask_to_continue('\nConvert again? (Y/N): ')
