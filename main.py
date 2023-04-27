002134c72716e6e8da8a385a2249478adf862367
paukstello <paukstelom@gmail.com>
2023-04-27 11:23:12 +0300

    Morse Code Alpha Created dict that shows relations between letters/numbers and symbols
    Create a simple text input and morse code output

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# Text to morse code converter
+from morse_code_dict import morse_code_dict
+
+running = True
+while running:
+    text_input = input('Enter text:\n').upper().strip()
+
+    morse_code_output = ''
+    for char in text_input:
+        if char in morse_code_dict.keys():
+            morse_code_output += morse_code_dict[char]
+    print(morse_code_output.rstrip())
+
